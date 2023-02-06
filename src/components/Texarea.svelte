aff20ff95a93b5e1399dd617f03a9f44a0b9724a
Caracolazza <120380236+Caracolazza@users.noreply.github.com>
2023-02-06 18:25:54 -0300

    add spacing text area

--- src/components/Texarea.svelte
+++ src/components/Texarea.svelte
@@ -19,8 +19,8 @@
   on:input={handleChange}
   id="result"
   class="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-  placeholder="what ingredients do you have?"
+  placeholder="Enter two ingredients, Example: Flour and milk"
   name="comment"
-  rows="5"
+  rows="10"
   cols="40"
 />
