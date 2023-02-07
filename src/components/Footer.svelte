31e637efa8d852f4d4b2e8b871e48e29679c5b06
Caracolazza <120380236+Caracolazza@users.noreply.github.com>
2023-02-06 23:59:16 -0300

    footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,11 @@
+<footer class="py-10">
+  <a class="underline underline-offset-1 pt-10" href="https://www.sebastianlagos.cl" target="_blank" rel="noopener noreferrer">Created by Sebastián Lagos</a>
+</footer>
+
+<style>
+  footer {
+    color: #fff;
+    text-align: center;
+    background-color:#3e3772;
+  }
+</style>
